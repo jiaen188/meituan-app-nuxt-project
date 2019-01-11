963d85c902f047b54a6f8181b05a92b414f2a528
gulifan <gulifan@dxy.cn>
2019-01-11 22:46:36 +0800

    feat:add navbar

--- components/public/header/index.vue
+++ components/public/header/index.vue
@@ -1,29 +1,34 @@
 <template>
   <el-row
-    :gutter="15"
+    :gutter="0"
     class="m-header">
-    <el-col :span="6">
+    <el-col :span="4">
       <geo />
     </el-col>
     <el-col :span="6">
       <user />
     </el-col>
-    <el-col :span="6">nav</el-col>
+    <el-col :span="14">
+      <navbar />
+    </el-col>
   </el-row>
 </template>
 
 <script>
 import Geo from './geo.vue'
 import User from './user.vue'
+import Navbar from './nav.vue'
 
 export default {
   components: {
     Geo,
-    User
+    User,
+    Navbar
   }
 }
 </script>
 
 <style lang="scss">
- @import '@/assets/css/public/layout.scss';
+  @import "@/assets/css/public/header/index.scss";
+  // @import "@/assets/css/public/layout.scss";
 </style>
