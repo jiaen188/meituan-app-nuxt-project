f792b5b41b38c7ea7a438182ea7c70e9ed9874d9
jiaen188 <18258335270@163.com>
2019-01-24 20:29:31 +0800

    feat:增加菜单部分

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,9 @@
       <el-col :span="5">
         <emenu />
       </el-col>
-      <el-col :span="19">2测试github账号</el-col>
+      <el-col :span="19">
+        <life />
+      </el-col>
     </el-row>
     <el-row>
       <el-col :span="24">3</el-col>
@@ -14,10 +16,12 @@
 
 <script>
 import Emenu from '@/components/index/menu.vue'
+import Life from '@/components/index/life.vue'
 
 export default {
   components: {
     Emenu,
+    Life,
   }
 }
 </script>
