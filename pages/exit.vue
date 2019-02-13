9eb615399d667b0cf30d9d653353d975d0298b97
jiaen188 <18258335270@163.com>
2019-02-13 10:30:10 +0800

    feat:exit退出功能

---
+++ pages/exit.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="" />
+</template>
+
+<script>
+export default {
+  layout: 'blank',
+  middleware: async (ctx) => { // 因为这个退出页面，没有需要用户的任何操作就要退出，所以放在中间件执行
+    let { status, data } = await ctx.$axios.get('/users/exit')
+    if (status === 200 && data && data.code === 0) {
+      window.location.href = '/'
+    }
+  }
+}
+</script>
