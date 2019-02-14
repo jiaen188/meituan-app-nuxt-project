0ab98a999b2f82b914441ed5d00534b00c5b3a73
jiaen188 <18258335270@163.com>
2019-02-14 15:41:56 +0800

    fix:删除无用引入

--- server/interface/geo.js
+++ server/interface/geo.js
@@ -1,7 +1,6 @@
 import Router from 'koa-router'
 import axios from './utils/axios'
 import Province from '../dbs/models/province'
-import { listenerCount } from 'cluster';
 
 let router = new Router({
   prefix: '/geo'
