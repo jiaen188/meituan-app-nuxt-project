516435153ba46389aff2ae7b9a545b03e68ad70c
jiaen188 <18258335270@163.com>
2019-03-07 11:11:14 +0800

    feat:products系列的组件完成

---
+++ components/products/crumbs.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="m-crumbs">
+    <el-bredcrumb separator=">">
+      <el-breadcrumb-item :to="{ path: '/' }">{{ $store.state.geo.position.city.length ? $store.state.geo.position.city.replace('市', '') : '杭州' }}美团</el-breadcrumb-item>
+      <el-breadcrumb-item>
+        <a href="/">{{ $store.state.geo.position.city.length ? $store.state.geo.positioin.city.replace('市', '') : '杭州' }}{{ decodeURIComponent(keyword) }}</a>
+      </el-breadcrumb-item>
+    </el-bredcrumb>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    keyword: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
