274af34b766eacda9de58b0823e22815e7b5cf20
jiaen188 <18258335270@163.com>
2019-03-08 16:36:47 +0800

    fix:nav tab的切换功能

--- components/products/list.vue
+++ components/products/list.vue
@@ -4,13 +4,14 @@
       <dd
         v-for="item in nav"
         :key="item.name"
-        :class="[item.name, item.active ? 's-nav-active' : '']">
+        :class="[item.name, item.active ? 's-nav-active' : '']"
+        @click="navSelect(item)">
         {{ item.txt }}
       </dd>
     </dl>
     <ul>
       <Item
-        v-for="(item, index) in list"
+        v-for="(item, index) in renderList"
         :key="index"
         :meta="item" />
     </ul>
@@ -55,13 +56,28 @@ export default {
       ]
     }
   },
+  computed: {
+    renderList() {
+      return this.list.map(item => ({ ...item }))
+    }
+  },
   async asyncData({ app }) {
     let { data } = await app.$axios.get('searchList')
     return { items: data.list }
   },
   methods: {
-    navSelect() {
-      console.log('select')
+    navSelect(item) {
+      this.nav.find(item => item.active).active = false
+      item.active = true
+      if (item.txt === '智能排序') {
+        console.log('智能排序')
+      } else if (item.txt === '价格最低') {
+        this.renderList.sort((a, b) => a.price - b.price)
+      } else if (item.txt === '人气最高') {
+        this.renderList.sort((a, b) => b.comment - a.comment)
+      } else {
+        this.renderList.sort((a, b) => b.rate - a.rate)
+      }
     }
   }
 }
