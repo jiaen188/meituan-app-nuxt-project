b8649812ccf5736c947e16eda0748ff316e612af
jiaen188 <18258335270@163.com>
2019-03-11 15:07:00 +0800

    feat:产品详情页面的面包屑

---
+++ components/detail/crumbs.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="m-crumbs">
+    <el-breadcrumb>
+      <el-breadcrumb-item :to="{ path: '/' }">{{$store.state.geo.position.city.length ? $store.state.geo.position.city.replace('市', '') : '杭州' }}美团</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{ paht: '/' }">{{$store.state.geo.position.city.length ? $store.state.geo.position.city.replace('市', '') : '杭州' }}{{ type }}</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{ paht: '/' }">
+        <a href="/">{{$store.state.geo.position.city.length ? $store.state.geo.position.city.replace('市', '') : '杭州' }}{{ decodeURIComponent(keyword) }}</a>
+      </el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: ''
+    },
+    keyword: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
