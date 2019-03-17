ef4a5e0e9d8dca9f1fa61383049163fd342797c8
jiaen188 <18258335270@163.com>
2019-03-17 17:22:34 +0800

    feat:购物车界面

---
+++ pages/cart.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="page-cart">
+    <el-row>
+      <el-col
+        v-if="1 || cart.length"
+        :span="24"
+        class="m-cart">
+        <list />
+        <p>
+          应付金额：<em class="money">¥{{ total }}</em>
+        </p>
+        <div class="post">
+          <el-button
+            type="primary"
+            @click="submit">提交订单</el-button>
+        </div>
+      </el-col>
+      <el-col
+        v-else
+        class="empty">购物车为空</el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import List from '@/components/cart/List.vue'
+
+export default {
+  components: {
+    List,
+  },
+  data() {
+    return {
+      cart: []
+    }
+  },
+  computed: {
+    total() {
+      let total = 0
+      this.cart.forEach(item => {
+        total += item.price * item.count
+      })
+      return total
+    }
+  },
+  methods: {
+    submit() {
+      console.log('submit')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import '@/assets/css/cart/index.scss';
+</style>
