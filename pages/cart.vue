4efbe8b97d16a70ee6b1cd4a0fef65910b0d3901
jiaen188 <18258335270@163.com>
2019-03-27 18:51:19 +0800

    feat:创建购物车

--- pages/cart.vue
+++ pages/cart.vue
@@ -44,8 +44,20 @@ export default {
     }
   },
   methods: {
-    submit() {
-      console.log('submit')
+    async submit() {
+      let { status, data: { code, id } } = await this.$axios.post('/order/createOrder', {
+        count: this.cart[0].count,
+        price: this.cart[0].price,
+        id: this.cartNo
+      })
+      if (status === 200 && code === 0) {
+        this.$alert(`恭喜您，已成功下单，订单号：${id}`, '下单成功', {
+          confirmButtonText: '确定',
+          callback: action => {
+            location.href = '/order'
+          }
+        })
+      }
     }
   },
   async asyncData(ctx) {
